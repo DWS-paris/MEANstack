36c2bf6cc16e2ab17dee6847cbeb3b7ba350a3ab
« Julien <j.noyer@digitalworkshop.fr>
2017-05-03 15:27:57 +0200

    modification du service

--- client_angular/src/app/services/mongodb/mongodb.service.ts
+++ client_angular/src/app/services/mongodb/mongodb.service.ts
@@ -13,8 +13,8 @@ export class MongodbService {
   ) {}
 
   // Définition des adresses de l'API => routes/api.js
-  private getCategoriesUrl = 'https://localhost:8080/api/tasks';
-  private editTaskUrl = 'https://localhost:8080/api/task';
+  private getCategoriesUrl = 'http://localhost:8080/api/tasks';
+  private editTaskUrl = 'http://localhost:8080/api/task';
 
 
 
